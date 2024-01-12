4b4753de1e10a9726b3d35060e5f38f59d608941
lordsarcastic <>
2024-01-12 19:07:29 +0100

    Feature: Implemented integration for GCR images and repository

--- README.md
+++ README.md
@@ -1,22 +1,163 @@
 # Getting started
-In this example, you will create a blueprint for `{blueprint}` that ingests {service} Packages from your {service} account into Port. You will then use a Python script to make API calls to {service} REST API to fetch the data from your account.
+In this example, you will create a blueprint for `gcrImage` and `gcrRepository` that ingests GCR images from your GCR account into Port. You will then use a Python script to make API calls to GCR REST API to fetch the data from your account.
 
 ## Blueprints
 Create the following blueprints in Port using the schemas:
 
+### Repository
+```json
+{
+  "identifier": "gcrRepository",
+  "description": "This blueprint represents a Container Registry Repository in our software catalog",
+  "title": "GCR Repository",
+  "icon": "GoogleCloudPlatform",
+  "schema": {
+    "properties": {
+      "description": {
+        "title": "Description",
+        "type": "string",
+        "description": "Repository Description"
+      },
+      "format": {
+        "title": "Format",
+        "type": "string",
+        "description": "Package Format",
+        "enum": [
+          "FORMAT_UNSPECIFIED",
+          "DOCKER",
+          "MAVEN",
+          "NPM",
+          "APT",
+          "YUM",
+          "PYTHON",
+          "KFP",
+          "GO"
+        ]
+      },
+      "labels": {
+        "title": "Labels",
+        "type": "object",
+        "description": "Labels with user-defined metadata"
+      },
+      "mode": {
+        "title": "Mode",
+        "description": "The mode configures the repository to serve artifacts from different sources.",
+        "type": "string",
+        "enum": [
+          "MODE_UNSPECIFIED",
+          "STANDARD_REPOSITORY",
+          "VIRTUAL_REPOSITORY",
+          "REMOTE_REPOSITORY"
+        ]
+      },
+      "createdAt": {
+        "title": "Created At",
+        "description": "Time repository was created",
+        "type": "string",
+        "format": "date-time"
+      },
+      "updatedAt": {
+        "title": "Updated At",
+        "description": "Last time repository was updated",
+        "type": "string",
+        "format": "date-time"
+      },
+      "kmsKeyName": {
+        "title": "KMS Key Name",
+        "description": "The Cloud KMS resource name of the customer managed encryption key that's used to encrypt the contents of the Repository",
+        "type": "string"
+      },
+      "size": {
+        "title": "Size",
+        "description": "Repository size in bytes",
+        "type": "number"
+      },
+      "satisfiesPhysicalZoneSeparation": {
+        "title": "Satisfies Physical Zone Separation",
+        "description": "Whether the repository satisfies physical zone separation.",
+        "type": "boolean"
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "aggregationProperties": {},
+  "relations": {}
+}
+```
 
-### `{Blueprint}`
-Python Script for Ingesting {service} {Blueprint} in Port
+### Image
 
 ```json
-{}
+{
+  "identifier": "gcrImage",
+  "description": "This blueprint represents a Container Registry Image in our software catalog",
+  "title": "GCR Image",
+  "icon": "GoogleCloudPlatform",
+  "schema": {
+    "properties": {
+      "uri": {
+        "title": "URI",
+        "type": "string",
+        "description": "Image URI"
+      },
+      "tags": {
+        "title": "Tags",
+        "type": "array",
+        "description": "Tags attached to this image."
+      },
+      "size": {
+        "title": "Size",
+        "description": "Image size in bytes",
+        "type": "number"
+      },
+      "uploadedAt": {
+        "title": "Uploaded At",
+        "description": "Time image was uploaded",
+        "type": "string",
+        "format": "date-time"
+      },
+      "mediaType": {
+        "title": "Media Type",
+        "description": "Media type of this image",
+        "type": "string"
+      },
+      "buildTime": {
+        "title": "Build Time",
+        "description": "Time image was built",
+        "type": "string",
+        "format": "date-time"
+      },
+      "updatedAt": {
+        "title": "Updated At",
+        "description": "Last time image was updated",
+        "type": "string",
+        "format": "date-time"
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "aggregationProperties": {},
+  "relations": {
+    "repository": {
+      "title": "Repository",
+      "description": "Repository this image is attached to",
+      "target": "gcrRepository",
+      "many": false,
+      "required": true
+    }
+  }
+}
 ```
 
 ## Running the Python script
 First clone the repository and cd into the work directory with:
 ```bash
-$ git clone {https git repo link}
-$ cd {repo folder}
+$ git clone https://github.com/port-labs/example-gcr-images.git
+$ cd example-gcr-images
 ```
 
 Install the needed dependencies within the context of a virtual environment with:
@@ -26,13 +167,16 @@ $ source venv/bin/activate
 $ pip3 install -r requirements.txt
 ```
 
-To ingest your data, you need to populate some environment variables. You can do that by either duplicating the `.example.env` file and renaming the copy as `.env`, then edit the values as needed; or run the commands below in your terminal:
+To ingest your data, you need to populate some environment variables. You should do that by duplicating the `.example.env` file and renaming the copy as `.env`, then edit the values as needed; or run the commands below in your terminal:
 
 ```bash
 export PORT_CLIENT_ID=port_client_id
 export PORT_CLIENT_SECRET=port_client_secret
-export {service}_ACCESS_TOKEN={service}_access_token
+export GCP_REGIONS=region1,region2,region3
+export GCP_PROJECT_ID=my-project-id
 ```
+
+In addition, you must set up your GCP credentials. You can do that by following the instructions on the [ADC documentation page](https://cloud.google.com/docs/authentication/provide-credentials-adc). Alternatively, you can use any of the authentication methods outlined in the [Python Client Library documentation](https://github.com/googleapis/google-api-python-client/blob/main/docs/auth.md).
 
 Then run the script with:
 ```bash
@@ -42,6 +186,46 @@ $ python app.py
 Each variable required are:
 - PORT_CLIENT_ID: Port Client ID
 - PORT_CLIENT_SECRET: Port Client secret
-- {service}_ACCESS_TOKEN: {service} access token
+- GCP_REGIONS: Comma separated list of GCP regions to fetch images from. If not set, defaults to "us-east1,us-central1".
+- GCP_PROJECT_ID: GCP project ID
 
-You can get the {service} token by following the instructions on the [link](https://developer.{service}.com/docs/authentication).
+List of all possible regions you can set for `GCP_REGIONS`:
+- asia-east1
+- asia-east2
+- asia-northeast1
+- asia-northeast2
+- asia-northeast3
+- asia-south1
+- asia-south2
+- asia-southeast1
+- asia-southeast2
+- australia-southeast1
+- australia-southeast2
+- europe-central2
+- europe-north1
+- europe-southwest1
+- europe-west1
+- europe-west2
+- europe-west3
+- europe-west4
+- europe-west6
+- europe-west8
+- europe-west9
+- europe-west10
+- europe-west12
+- us-west1
+- us-west2
+- us-west3
+- us-west4
+- us-central1
+- us-east1
+- us-east4
+- us-east5
+- us-south1
+- northamerica-northeast1
+- northamerica-northeast2
+- southamerica-east1
+- southamerica-east2
+- me-central1
+- me-central2
+- me-west1
--- app.py
+++ app.py
@@ -2,24 +2,66 @@ import asyncio
 
 import aiohttp
 import requests
+import dotenv
 from loguru import logger
+from google.cloud import artifactregistry_v1
 from pydantic_settings import BaseSettings
 
+dotenv.load_dotenv()
 
 class Environment(BaseSettings):
     PORT_API_URL: str = "https://api.getport.io/v1"
     PORT_CLIENT_ID: str
     PORT_CLIENT_SECRET: str
-    # define service envs here
-    # SERVICE_TOKEN: str
-
-    class Config:
-        env_file = ".env"
+    GCP_REGIONS: str
+    GCP_PROJECT_ID: str
 
 ENVIRONMENT = Environment()
+GCP_REGIONS = ENVIRONMENT.GCP_REGIONS.split(",")
 
-# define blueprint ids here
-SERVICE_BLUEPRINT = "serviceItem"
+REPOSITORY_BLUEPRINT = "gcrRepository"
+IMAGE_BLUEPRINT = "gcrImage"
+ALL_GCR_REGIONS = {
+    "asia-east1",
+    "asia-east2",
+    "asia-northeast1",
+    "asia-northeast2",
+    "asia-northeast3",
+    "asia-south1",
+    "asia-south2",
+    "asia-southeast1",
+    "asia-southeast2",
+    "australia-southeast1",
+    "australia-southeast2",
+    "europe-central2",
+    "europe-north1",
+    "europe-southwest1",
+    "europe-west1",
+    "europe-west2",
+    "europe-west3",
+    "europe-west4",
+    "europe-west6",
+    "europe-west8",
+    "europe-west9",
+    "europe-west10",
+    "europe-west12",
+    "us-west1",
+    "us-west2",
+    "us-west3",
+    "us-west4",
+    "us-central1",
+    "us-east1",
+    "us-east4",
+    "us-east5",
+    "us-south1",
+    "northamerica-northeast1",
+    "northamerica-northeast2",
+    "southamerica-east1",
+    "southamerica-east2",
+    "me-central1",
+    "me-central2",
+    "me-west1",
+}
 
 ## Get Port Access Token
 credentials = {"clientId": ENVIRONMENT.PORT_CLIENT_ID, "clientSecret": ENVIRONMENT.PORT_CLIENT_SECRET}
@@ -30,6 +72,8 @@ access_token = token_response.json()["accessToken"]
 
 # You can now use the value in access_token when making further requests
 headers = {"Authorization": f"Bearer {access_token}"}
+
+artifact_registry_client = artifactregistry_v1.ArtifactRegistryClient()
 
 
 async def add_entity_to_port(
@@ -65,11 +109,121 @@ async def add_entity_to_port(
         logger.info("Ingesting {blueprint_id} entity to port failed, skipping...")
     logger.info(f"Added entity to Port: {entity_object}")
 
+async def ingest_repositories(session: aiohttp.ClientSession, repository: artifactregistry_v1.Repository):
+    class_format = {
+        artifactregistry_v1.Repository.Format.DOCKER: "DOCKER",
+        artifactregistry_v1.Repository.Format.MAVEN: "MAVEN",
+        artifactregistry_v1.Repository.Format.NPM: "NPM",
+        artifactregistry_v1.Repository.Format.APT: "APT",
+        artifactregistry_v1.Repository.Format.YUM: "YUM",
+        artifactregistry_v1.Repository.Format.GO: "GO",
+        artifactregistry_v1.Repository.Format.PYTHON: "PYTHON",
+        artifactregistry_v1.Repository.Format.FORMAT_UNSPECIFIED: "FORMAT_UNSPECIFIED",
+        artifactregistry_v1.Repository.Format.KFP: "KFP",
+    }
+    mode_format = {
+        artifactregistry_v1.Repository.Mode.MODE_UNSPECIFIED: "MODE_UNSPECIFIED",
+        artifactregistry_v1.Repository.Mode.REMOTE_REPOSITORY: "REMOTE_REPOSITORY",
+        artifactregistry_v1.Repository.Mode.STANDARD_REPOSITORY: "STANDARD_REPOSITORY",
+        artifactregistry_v1.Repository.Mode.VIRTUAL_REPOSITORY: "VIRTUAL_REPOSITORY",
+    }
+    repository_object = {
+        "identifier": repository.name,
+        "title": repository.name.split("/")[-1],
+        "properties": {
+            "description": repository.description,
+            "format": class_format.get(repository.format_),
+            "labels": dict(repository.labels),
+            "mode": mode_format.get(repository.mode),
+            "createdAt": repository.create_time.isoformat(),
+            "updatedAt": repository.update_time.isoformat(),
+            "kmsKey": repository.kms_key_name,
+            "size": repository.size_bytes,
+            "satisfiesPhysicalZoneSeparation": repository.satisfies_pzs,
+        },
+    }
+    logger.info(f"Adding repository to Port: {repository_object}")
+    await add_entity_to_port(session, REPOSITORY_BLUEPRINT, repository_object)
+
+
+async def ingest_images(session: aiohttp.ClientSession, image: artifactregistry_v1.DockerImage, repository: artifactregistry_v1.Repository):
+    image_object = {
+        "identifier": image.name,
+        "title": image.name.split("/")[-1],
+        "properties": {
+            "uri": image.uri,
+            "tags": list(image.tags),
+            "size": image.image_size_bytes,
+            "uploadedAt": image.upload_time.isoformat(),
+            "mediaType": image.media_type,
+            "buildTime": image.build_time.isoformat(),
+            "updatedAt": image.update_time.isoformat(),
+        },
+        "relations": {
+            "repository": repository.name
+        }
+    }
+    logger.info(f"Adding image to Port: {image_object}")
+    await add_entity_to_port(session, IMAGE_BLUEPRINT, image_object)
+
+async def fetch_repositories(session: aiohttp.ClientSession, gcp_client: artifactregistry_v1.ArtifactRegistryClient, project_id: str, region: str):
+    """A function to fetch repositories from GCR
+
+    Params
+    --------------
+    session: aiohttp.ClientSession
+        The aiohttp session object
+
+    region: str
+        The GCR region to fetch repositories from
+
+    Returns
+    --------------
+    None
+    """
+    logger.info(f"Fetching repositories from GCR: {region}")
+    repository_request = artifactregistry_v1.ListRepositoriesRequest(
+        parent=f"projects/{project_id}/locations/{region}",
+        page_size=1
+    )
+    response = gcp_client.list_repositories(request=repository_request)
+    for page in response.pages:
+        logger.info(f"Fetching page of repositories from GCR: {region}")
+        for repository in page.repositories:
+            yield repository
+
+
+async def fetch_images(session: aiohttp.ClientSession, gcp_client: artifactregistry_v1.ArtifactRegistryClient, project_id: str, region: str, repository: artifactregistry_v1.Repository):
+    """
+    Fetch images from GCR
+    """
+    logger.info(f"Fetching images from GCR: {region} for repository: {repository.name}")
+    image_request = artifactregistry_v1.ListDockerImagesRequest(
+        parent=repository.name,
+        page_size=1
+    )
+    response = gcp_client.list_docker_images(request=image_request)
+    for page in response.pages:
+        logger.info(f"Fetching page of images for repository: {repository.name}")
+        for image in page.docker_images:
+            yield image
 
 async def main():
+    logger.info("Validating GCP regions")
+    for region in GCP_REGIONS:
+        if region not in ALL_GCR_REGIONS:
+            raise ValueError(f"Invalid GCP region: {region}")
     logger.info("Starting Port integration")
     async with aiohttp.ClientSession() as session:
-        pass
+        for region in GCP_REGIONS:
+            logger.info(f"Fetching repositories from GCR: {region}")
+            async for repository in fetch_repositories(session, artifact_registry_client, ENVIRONMENT.GCP_PROJECT_ID, region):
+                await ingest_repositories(session, repository)
+                async for image in fetch_images(session, artifact_registry_client, ENVIRONMENT.GCP_PROJECT_ID, region, repository):
+                    await ingest_images(session, image, repository)
+    logger.info("Finished Port integration")
+
+    
 
 
 if __name__ == "__main__":
